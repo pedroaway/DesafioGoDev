2df7222cdd70fdfcd52e7d88674fafba8ccdbe09
pedroaway <ppgabriell00@gmail.com>
2021-02-24 02:25:47 -0300

    lógica de extração de dados desenvolvida

--- MainWindow.py
+++ MainWindow.py
@@ -1,4 +1,5 @@
 import sys
+from PyQt5 import uic, QtWidgets
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from interface import Ui_MainWindow
@@ -38,23 +39,32 @@ class MainWindow:
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.main_win)
 
+###BOTÕES PARA MUDAR AS PÁGINAS NA APLICAÇÃO
         self.ui.TELAS.setCurrentWidget(self.ui.page1)
-
         self.ui.botao1.clicked.connect(self.showtela1)
         self.ui.botao2.clicked.connect(self.showtela2)
         self.ui.botao3.clicked.connect(self.showtela3)
         self.ui.botao4.clicked.connect(self.showtela4)
         self.ui.botao5.clicked.connect(self.showtela5)
+###BOTAO TELA 1
+        self.ui.botao4_5.clicked.connect(self.informacoes1)
 
+
+
+###EXTRAÇÃO DE DADOS
+    def informacoes1(self):
+        nomeparticipante = self.ui.lineEdit_8.text()
+        sobrenomeparticipante = self.ui.lineEdit_9.text()
+        print(nomeparticipante)
+        print(sobrenomeparticipante)
+
+###FUNÇÕES QUE CONECTAM OS BOTÕES COM AS PÁGINAS E FAZEM A MUDANÇA
     def show(self):
         self.main_win.show()
-
     def showtela1(self):
         self.ui.TELAS.setCurrentWidget(self.ui.page1)
-
     def showtela2(self):
         self.ui.TELAS.setCurrentWidget(self.ui.page2)
-
     def showtela3(self):
         self.ui.TELAS.setCurrentWidget(self.ui.page3)
     def showtela4(self):
@@ -62,6 +72,13 @@ class MainWindow:
     def showtela5(self):
         self.ui.TELAS.setCurrentWidget(self.ui.page5)
 
+
+##funções das telas
+
+
+
+    def extrair_dados(self):
+        NomeParticipante = self.ui
 
 
 
@@ -71,3 +88,4 @@ if __name__ == "__main__":
     main_win = MainWindow()
     main_win.show()
     sys.exit(app.exec_())
+
